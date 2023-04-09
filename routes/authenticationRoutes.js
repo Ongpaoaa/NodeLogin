7e5eedfd557b1bb3f198827217443de407ceffe7
Solnar <49712045+Ongpaoaa@users.noreply.github.com>
2023-04-09 09:48:56 +0700

    Clean code

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -5,128 +5,102 @@ const quest = mongoose.model("quests");
 module.exports = app => {
 
 //Routes
-
-    app.post('/account/login', async (req, res) => {
-
-        const { rUsername, rPassword} = req.body;
-        if(rUsername == null || rPassword == null)
-        {
-            console.log("rUsername,rPassword");
-            res.send("Invalid credentials")
-            return;
-        }
-
-    var userAccount = await Account.findOne({ username : rUsername});
-        if(userAccount != null){
-            
-        if(rPassword == userAccount.password){
-                userAccount.lastAuthentication = Date.now();
-                await userAccount.save();
-                
-                res.send(userAccount);
-                return;
-            }
-        }
-
-        res.send('Invalid credentials');
+//login api
+app.post('/account/login', async (req, res) => {
+    const { rUsername, rPassword } = req.body; //use 2 data for login rUsername, rPassword
+    if (!rUsername || !rPassword) { //if 2 data are null end api
+      res.send("Invalid credentials");
+      return;
+    }
+  
+    const userAccount = await Account.findOne({ username: rUsername }); //find username in Account database
+    if (userAccount) {  //if find user account  
+      if (rPassword === userAccount.password) { //check password
+        userAccount.lastAuthentication = Date.now(); //update date
+        await userAccount.save();
+        res.send(userAccount); //send user info
         return;
-    })
+      }
+    }
+  
+    res.send('Invalid credentials'); //if can't find user
+  });
 
+    //create account
     app.post('/account/create', async (req, res) => {
-
-        console.log(req.body);
-        const {rEmail, rUsername, rPassword} = req.body;
-        if(rEmail == null || rUsername == null || rPassword == null)
-        {
-            
-            res.send("Invalid credentials")
-            return;
+        const { rEmail, rUsername, rPassword } = req.body;
+      
+        if (!rEmail || !rUsername || !rPassword) {
+          res.send('Invalid credentials');
+          return;
         }
-
-    var userAccount = await Account.findOne({ username : rUsername});
-    var emailAccount = await Account.findOne({ email : rEmail});
-
-        if(emailAccount == null){
-            if(userAccount == null){
-                console.log("create new account");
-                const foodtype = ["spicy","fried","sweet","bread","meat","vegetable","fruit"];
-                foodtype.sort(() => Math.random() - 0.5);
-                const ffood = [foodtype.shift(),foodtype.shift(),foodtype.shift()];
-                var dfood = [foodtype.pop(),foodtype.pop(),foodtype.pop()];
-                var newAccount = new Account({
-                    email : rEmail,
-                    username : rUsername,
-                    password : rPassword,
-                    item : ({}),
-                    pending: ({}),
-                    wOof: ({hp: 120, favoritef: ffood, dislike: dfood, type: "", en: 5, str: 5, int:5}),
-                    quest: ([]),
-                    finishedQ: ({}),
-
-                    lastAuthentication : Date.now()
-                });
-                await newAccount.save();
-
-                res.send(newAccount);
-                return;
-            } else {
-                res.send("Username is already taken")
-            }
+      
+        const userAccount = await Account.findOne({ username: rUsername });
+        const emailAccount = await Account.findOne({ email: rEmail });
+      
+        if (emailAccount) {
+          res.send('Email is already taken');
+          return;
         }
-        else {
-            res.send("Email is already taken")
+      
+        if (userAccount) {
+          res.send('Username is already taken');
+          return;
         }
-        res.send('Invalid credentials');
-        return;
-        })
+      
+        const foodTypes = ['spicy', 'fried', 'sweet', 'bread', 'meat', 'vegetable', 'fruit'];
+        const favoriteFoods = [foodTypes.shift(), foodTypes.shift(), foodTypes.shift()];
+        const dislikedFoods = [foodTypes.pop(), foodTypes.pop(), foodTypes.pop()];
+      
+        const newAccount = new Account({
+          email: rEmail,
+          username: rUsername,
+          password: rPassword,
+          wOof: {
+            hp: 120,
+            favoritef: favoriteFoods,
+            dislike: dislikedFoods,
+            type: '',
+            en: 5,
+            str: 5,
+            int: 5
+          },
+          lastAuthentication: Date.now()
+        });
+      
+        await newAccount.save();
+        res.send(newAccount);
+      });
+      
 
-    app.post('/account/give', async (req, res) => {
-        const {rUsername, rItemName} = req.body;
-        if(rUsername == null || rItemName == null )
-        {
-            res.send("Not enough info")
-            return;
-        } //test
-        var userAccount = await Account.findOne({ username : rUsername});
-        var dulp = parseInt(userAccount.item[rItemName]);
-        var Names = 'item.'+rItemName;
-
-        if (isNaN(dulp) == true)
-        {
-            var obj = {};
-                obj[Names] = 1;
-            const ress = await Account.updateOne(
-                {username : rUsername}, 
-                {$set: obj});
-            res.send("complete1");
-        }
-        else
-        {
-            var obj2 = {};
-                obj2[Names] = dulp+1;
-            dulp += 1;
-            const ress = await Account.updateOne(
-                {username : rUsername}, 
-                {$set: obj2});
-            res.send(obj2);
-        }
+        //api given item to player
+        app.post('/account/give', async (req, res) => {
+            const { rUsername, rItemName } = req.body;
         
-        // if(userAccount == null)
-        // {
-        //     res.send("There is no user")
-        //     return;
-        // }
-        // else
-        // {
-        //     Account.updateOne({$set:{ "ticket": 1}});
-        //     Account.save();   
-        //     res.send("Sucess");
-        // }
+            if (!rUsername || !rItemName) {
+                res.send("Not enough info");
+                return;
+            }
+        
+            const userAccount = await Account.findOne({ username: rUsername });
+            const itemAmount = parseInt(userAccount.item[rItemName] || 0);
+            const Names = 'item.' + rItemName;
+        
+            const updateQuery = {
+                $set: { [Names]: itemAmount + 1 }
+            };
+        
+            const updateResult = await Account.updateOne({ username: rUsername }, updateQuery);
+        
+            if (updateResult.nModified === 0) {
+                res.send("Failed to update item amount");
+            } else {
+                res.send(updateQuery.$set);
+            }
+        });
 
-        })
 
-
-        app.post('/account/getdata', async (req, res) => {
+        app.post('/account/getdata', async (req, res) => { //get data player
             const {rUsername} = req.body;
             if(rUsername == null  )
             {
@@ -140,46 +114,46 @@ module.exports = app => {
         })
 
         app.post('/account/sentgift', async (req, res) => {
-            const {rSentedperson, rRecieveperson, rGift} = req.body;
-            if(rSentedperson == null , rRecieveperson == null , rGift == null)
+            const { rSentedperson, rRecieveperson, rGift } = req.body;
+            if (!rSentedperson || !rRecieveperson || !rGift) {
+              res.send("Not enough info");
+              return;
+            }
+          
+            const sentedAccount = await Account.findOne({ username: rSentedperson });
+            const recievedAccount = await Account.findOne({ username: rRecieveperson });
+          
+            const dulp = parseInt(recievedAccount.item[rGift]);
+            const dulp2 = parseInt(sentedAccount.item[rGift]);
+          
+            const obj2 = { [`item.${rGift}`]: dulp2 - 1 };
+            await Account.updateOne({ username: rSentedperson }, { $set: obj2 });
+          
+            if (isNaN(dulp)) {
+              const obj = { [`pending.${rSentedperson}`]: rGift };
+              await Account.updateOne({ username: rRecieveperson }, { $set: obj });
+              res.send("complete1");
+            }
+          });
+
+
+        app.post('/account/recievegift', async (req, res) => {
+            const {rRecieveperson, rSentedperson} = req.body; //require 2 data
+            if(rSentedperson == null || rRecieveperson == null) //check 3 data
             {
-                res.send("Not enough info")
-                return;
+              res.send("Not enough info");
+              return;
             } 
-            var sentedAccount = await Account.findOne({ username : rSentedperson});
-            var recievedAccount = await Account.findOne({ username : rRecieveperson});
-            var dulp = parseInt(recievedAccount.item[rGift]);
-            var dulp2 = parseInt(sentedAccount.item[rGift]);
-            var Names = 'item.'+rGift;
-            var obj2 = {};
-                obj2[Names] = dulp2 -= 1;
-            const ress = await Account.updateOne(
-                {username : rSentedperson}, 
-                {$set: obj2});
-            console.log(Names);
-            if (isNaN(dulp) == true)
-            {
-                var Names = 'pending.'+ rGift;
-                var obj = {};
-                    obj[Names] = rSentedperson;
-                const ress = await Account.updateOne(
-                    {username : rRecieveperson}, 
-                    {$set: obj});
-                res.send("complete1");
+            try {
+              const recievedAccount = await Account.findOne({ username: rRecieveperson });
+              delete recievedAccount.pending[rSentedperson];
+              await recievedAccount.save();
+              console.log(recievedAccount);
+              res.send("Gift received successfully.");
+            } catch (err) {
+              console.error(err);
+              res.status(500).send("Internal server error.");
             }
-            else
-            {
-                var Names = 'pending.'+rGift;
-                var obj2 = {};
-                    obj[Names] = rSentedperson;
-                dulp += 1;
-                const ress = await Account.updateOne(
-                    {username : rRecieveperson}, 
-                    {$set: obj2});
-                res.send(obj2);
-            }
-
-        })
-
+          });
     
 }
